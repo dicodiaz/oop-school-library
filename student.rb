9f8c6668bcc582dceb6fcf604820db166155531b
Dico Diaz <dicodiaz@gmail.com>
2023-05-17 15:35:32 -0500

    Create Classroom and its relationship with Student

--- student.rb
+++ student.rb
@@ -1,14 +1,19 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom&.students&.delete(self)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
   end
 end
--- tests.rb
+++ tests.rb
@@ -1,9 +1,10 @@
 require './student'
 require './teacher'
+require './classroom'
 
 puts '', 'Student', ''
 
-p student1 = Student.new('2B', 7, 'Timmy', parent_permission: false)
+p student1 = Student.new(7, 'Timmy', parent_permission: false)
 p student1.play_hooky
 p student1.can_use_services?
 p student1.id
@@ -28,3 +29,14 @@ p capitalized_person.correct_name
 trimmed_capitalized_person = TrimmerDecorator.new(capitalized_person)
 p trimmed_capitalized_person.correct_name
 p trimmed_capitalized_person.nameable
+
+puts '', 'Classroom', ''
+
+classroom1 = Classroom.new('2A')
+classroom2 = Classroom.new('2B')
+
+student2 = Student.new(7, 'Jimmy', parent_permission: false)
+
+p classroom1.add_student(student1).add_student(student2)
+student2.classroom = classroom2
+p student2, classroom1, classroom2
