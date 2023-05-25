6becc7982479b00430976e28b4a1abda551aeb71
Rashed Arman <rashedarman21@gmail.com>
2023-05-26 03:17:06 +0600

    feat: implement fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -12,4 +12,12 @@ class Solver
 
     str.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
